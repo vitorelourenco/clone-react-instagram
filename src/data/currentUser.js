40d31a60094d3e2f6138bb981a9d1059838f99b7
Bruna Topan <brutopanb@gmail.com>
2021-05-04 16:35:37 -0300

    Preparando review

--- src/data/currentUser.js
+++
@@ -1,7 +0,0 @@
-const currentUser = {
-  username: 'catanacomics',
-  imgURL: 'assets/img/catanacomics.svg',
-  name: 'Catana'
-}
-
-export default currentUser;
--- src/post-content.js
+++
@@ -1,7 +0,0 @@
-export default function PostContent({contentImgURL}){
-  return (
-    <div className="conteudo">
-      <img alt="" src={contentImgURL} />
-    </div>
-  );
-}
